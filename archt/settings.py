97e251df85b1e3bed199967a19c27e8af9108663
MingquanLiang <shevnko@163.com>
2015-12-28 14:16:54 +0800

    require login

--- archt/settings.py
+++ archt/settings.py
@@ -98,6 +98,7 @@ else:
         }
 
 
+LOGIN_URL = '/accounts/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -112,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
